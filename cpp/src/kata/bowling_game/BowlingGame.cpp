58f9198007df6d41a8ebf912077e32d734c9a9fc
Damien Menanteau <damien.menanteau@gmail.com>
2021-01-26 16:16:41 +0100

    Adjust C++ bootstrap files

---
+++ cpp/src/kata/bowling_game/BowlingGame.cpp
@@ -0,0 +1,15 @@
+
+#include <kata/bowling_game/BowlingGame.hpp>
+
+#include <vector>
+
+namespace kata {
+namespace bowling_game {
+
+int bowlingScore(const std::vector<int> & rolls)
+{
+	return 0;
+}
+
+} // namespace bowling_game
+} // namespace kata
