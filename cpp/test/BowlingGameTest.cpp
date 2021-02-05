4e5a4d5d56b0f5392c0e201942a4832ef39798a4
Damien Menanteau <damien.menanteau@gmail.com>
2021-02-05 18:37:38 +0100

    Remove redundant function implementation

--- cpp/test/BowlingGameTest.cpp
+++ cpp/test/BowlingGameTest.cpp
@@ -5,12 +5,9 @@
 
 #include <vector>
 
-int bowlingScore(const std::vector<int>& rolls)
-{
-	return 0;
-}
+using namespace kata::bowling_game;
 
 TEST(BowlingGame, DISABLED_acceptance_test)
 {
-	EXPECT_EQ(133, bowlingScore(std::vector<int>{1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6}));
+   EXPECT_EQ(133, bowlingScore(std::vector<int>{1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6}));
 }
