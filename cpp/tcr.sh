e0ea73deba1842adecd10b96ee97d0775608d769
Damien Menanteau <damien.menanteau@gmail.com>
2021-03-19 18:35:53 +0100

    Add TCR scripts

---
+++ cpp/tcr.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+work_dir="./build"
+script_dir="../scripts"
+
+cd ${work_dir} || exit 1
+
+${script_dir}/buildIt.sh && \
+(${script_dir}/test.sh && ${script_dir}/commit.sh || ${script_dir}/revert.sh)
