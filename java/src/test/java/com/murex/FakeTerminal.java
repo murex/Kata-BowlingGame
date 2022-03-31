947c5971aa1730c2d355ab67e5337b929590167d
pdalesme <Patrice.DALESME@murex.com>
2022-03-31 14:48:59 +0200

    TCR

--- java/src/test/java/com/murex/FakeTerminal.java
+++ java/src/test/java/com/murex/FakeTerminal.java
@@ -3,12 +3,14 @@ package com.murex;
 import org.junit.Test;
 
 public class FakeTerminal implements Terminal {
+    private String input;
+
     @Override
     public void print(String input) {
-
+        this.input = input;
     }
 
     String getPrintedOutput() {
-        return "";
+        return this.input;
     }
 }
