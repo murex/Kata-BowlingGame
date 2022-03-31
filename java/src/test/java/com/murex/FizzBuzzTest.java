0cb7f0ed9f40b4f602eeaa6be9ed0ef5b625ca37
Philippe Bourgau <philippe.bourgau@murex.com>
2022-03-31 15:56:39 +0200

    TCR

--- java/src/test/java/com/murex/FizzBuzzTest.java
+++ java/src/test/java/com/murex/FizzBuzzTest.java
@@ -26,7 +26,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-//import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.*;
 
 public class FizzBuzzTest {
     @Test
