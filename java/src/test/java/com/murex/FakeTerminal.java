423e876bcc31eed2763f8e53cc4596727efa044d
NADIF Antoine <Antoine.NADIF@murex.com>
2022-03-31 14:52:22 +0200

    TCR

--- java/src/test/java/com/murex/FakeTerminal.java
+++ java/src/test/java/com/murex/FakeTerminal.java
@@ -1,16 +1,14 @@
 package com.murex;
 
-import org.junit.Test;
-
 public class FakeTerminal implements Terminal {
-    private String input;
+    private String text;
 
     @Override
-    public void print(String input) {
-        this.input = input;
+    public void print(String text) {
+        this.text = text;
     }
 
     String getPrintedOutput() {
-        return this.input;
+        return this.text;
     }
 }
