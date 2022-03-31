ca1128aeb18abce65a8e798b2d4d3d607a0a9518
Philippe Bourgau <philippe.bourgau@murex.com>
2022-03-31 14:18:39 +0200

    TCR

--- java/src/main/java/com/murex/FizzBuzz.java
+++ java/src/main/java/com/murex/FizzBuzz.java
@@ -47,7 +47,7 @@ class FizzBuzz {
         return result.toString();
     }
 
-    public static void main(String... args) {
+    public static void main(String[] args) {
         System.out.print(upTo(100));
     }
 
