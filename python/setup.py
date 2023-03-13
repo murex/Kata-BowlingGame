d99fc8b6c7e8e39c8ad9de1cfbeb1f11c148ac41
Damien Menanteau <damien.menanteau@gmail.com>
2023-03-13 16:06:52 +0100

    Move all setup from setup.cfg to setup.py
    - reason: PyCharm is able to use setup.py's content to initialise the project when opening the directory, while it does not seem to work with setup.cfg

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='bowling-game',
+    name='bowling_game',
+    version='0.0.1',
+    python_requires='>=3.6',
     extras_require=dict(tests=['pytest']),
     packages=find_packages(where="src"),
     package_dir={"": "src"},
