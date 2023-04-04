8254b959fa67f155cfc938485ed0893d69f3b2ac
Damien Menanteau <damien.menanteau@gmail.com>
2023-04-04 18:18:35 +0200

    Change python test framework from unittest to pytest

--- python/tests/bowling_game_test.py
+++ python/tests/bowling_game_test.py
@@ -1,14 +1,10 @@
-import unittest
+import pytest
 
 from bowling_game.bowling_game import bowling_score
 
 
-class BowlingGameTest(unittest.TestCase):
+class TestBowlingGame:
 
-    @unittest.skip("test currently disabled")  # Comment or remove this line to enable this test case
-    def test_acceptance_test(self):
-        self.assertEqual(133, bowling_score(1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6))
-
-
-if __name__ == "__main__":
-    unittest.main()
+    @pytest.mark.skip(reason="test currently disabled")  # Comment or remove this line to enable this test case
+    def test_acceptance_test(self) -> None:
+        assert (133, bowling_score(1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6))
