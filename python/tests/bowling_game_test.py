5434cdd096f97087fce5147a907a13995c3ad8ea
Damien Menanteau <damien.menanteau@gmail.com>
2023-04-14 12:31:24 +0200

    Fix error on assertion in python bootstrap acceptance test

--- python/tests/bowling_game_test.py
+++ python/tests/bowling_game_test.py
@@ -7,4 +7,4 @@ class TestBowlingGame:
 
     @pytest.mark.skip(reason="test currently disabled")  # Comment or remove this line to enable this test case
     def test_acceptance_test(self) -> None:
-        assert (133, bowling_score(1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6))
+        assert (133 == bowling_score(1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6))
