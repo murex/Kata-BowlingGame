eeae57d4a2e11f40cb60b3cca81d25cea5f137a0
Damien Menanteau <damien.menanteau@gmail.com>
2023-07-17 14:58:10 +0200

    Update dependencies

--- cpp/cpp_easy_setup.sh
+++ cpp/cpp_easy_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #
-# Copyright (c) 2022 Murex
+# Copyright (c) 2023 Murex
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
--- go/go.mod
+++ go/go.mod
@@ -1,6 +1,6 @@
 module github.com/murex/kata/bowling_game
 
-go 1.18
+go 1.20
 
 require github.com/stretchr/testify v1.8.4
 
