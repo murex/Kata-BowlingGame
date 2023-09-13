49d4365bf0e224a68d70ec1bbfdcbe0853dc4685
Damien Menanteau <damien.menanteau@gmail.com>
2023-09-13 18:19:19 +0200

    Upgrade TCR version to 0.28.0

--- tcr/tcr.sh
+++ tcr/tcr.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #
 # Copyright (c) 2023 Murex
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -110,7 +110,7 @@ download_and_expand_archive() {
   output_dir="$3"
 
   trace_info "Downloading ${archive_file}"
-  
+
   curl --silent \
        --fail \
        --dump-header "${output_dir}/${archive_file}".response \
