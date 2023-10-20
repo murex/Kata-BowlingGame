180703a1e8ba643e6179745b9c73f851718e34a2
Damien Menanteau <damien.menanteau@gmail.com>
2023-10-20 18:17:55 +0200

    Tune mix configuration for generating XUnit report

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -11,6 +11,12 @@ defmodule BowlingGame.MixProject do
     ]
   end
 
+  def cli do
+    [
+      default_env: :test
+    ]
+  end
+
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
@@ -21,8 +27,7 @@ defmodule BowlingGame.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:junit_formatter, "~> 3.3", only: [:test]},
     ]
   end
 end
