2ae23176ab2e3d2868bce656df30f6b90a24119c
Damien Menanteau <damien.menanteau@gmail.com>
2023-11-08 18:55:12 +0100

    Configure TCR to use parallel-lint tool as build command

--- php/php-check.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Utility script running "php -lf" on every php file under src and tests directories
-# This is to overcome the limitation of php command being able to check only 1 file
-
-find ./src ./tests -name "*.php" -print0 | xargs -0 -n1 -P8 php -lf
