ec3b1cbc4e467a533e890b9dd225045b46b0698c
Damien Menanteau <damien.menanteau@gmail.com>
2024-01-21 16:46:59 +0000

    Setup haskell test file to generate both JUnit.xml file and trace report

--- haskell/test/Tests.hs
+++ haskell/test/Tests.hs
@@ -9,19 +9,19 @@ import Test.Hspec.JUnit
 import System.Environment (setEnv)
 
 main :: IO ()
--- main = hspecWith defaultConfig {configFailFast = True} specs
-
 main = do
   -- setup JUnit file
   setEnv "JUNIT_ENABLED" "1"
   setEnv "JUNIT_OUTPUT_DIRECTORY" ".stack-work/report"
   setEnv "JUNIT_SUITE_NAME" "bowling-game"
 
-  -- hspecWith defaultConfig {configFailFast = True} specs
-  hspecJUnitWith defaultConfig {configFailFast = True} specs
-  -- hspecJUnit specs
+  -- Until we figure out a way to send test results simustaneously in console and in JUnit file
+  -- we run the tests twice
+  hspecJUnitWith defaultConfig {configFailFast = False} specs
+  hspecWith defaultConfig {configFailFast = False} specs
 
 specs :: Spec
+
 -- Replace "xdecribe" with "describe" below to enable acceptance tests
 specs = xdescribe "acceptance tests" $ for_ acceptanceTestCases test
   where
@@ -39,11 +39,6 @@ data Case = Case
 acceptanceTestCases :: [Case]
 acceptanceTestCases =
   [ Case
-      { description = "a bit of everything",
-        input = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6],
-        expected = 133
-      },
-    Case
       { description = "no bonus - all 1",
         input = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         expected = 20
@@ -57,5 +52,10 @@ acceptanceTestCases =
       { description = "strikes - all 10",
         input = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
         expected = 300
+      },
+    Case
+      { description = "a bit of everything",
+        input = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6],
+        expected = 133
       }
   ]
