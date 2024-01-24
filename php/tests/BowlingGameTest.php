8a91c3572f4b01b6e89fc2e7b043e30dcfdcf40e
Damien Menanteau <damien.menanteau@gmail.com>
2024-01-24 15:14:54 +0100

    Add dev container configuration for PHP language

--- php/tests/BowlingGameTest.php
+++ php/tests/BowlingGameTest.php
@@ -38,5 +38,4 @@ class BowlingGameTest extends TestCase
         $bowling = new BowlingGame();
         $this->assertEquals(133, $bowling->score(1,4, 4,5, 6,4, 5,5, 10, 0,1, 7,3, 6,4, 10, 2,8,6));
     }
-
 }
